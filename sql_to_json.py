abd6a6bf9880103515edc08de8774d1cd338d81a
wloszynski <adrian@wloszynski.pl>
2021-01-14 14:27:34 +0100

    updating script for converting sql to json

--- sql_to_json.py
+++ sql_to_json.py
@@ -2,43 +2,83 @@ import sqlite3
 import datetime
 import json
 
+categories = ['all', 'javascript', 'html', 'php', 'ruby',  'python','java', 'net', 'scala', 'c', 'mobile','testing', 'devops', 'ux','pm', 'game', 'analytics', 'security', 'data', 'go', 'sap', 'support','other']
+# categories = ['all', 'javascript']
+
+
 conn = sqlite3.connect('skill_counter.sqlite')
 cur = conn.cursor()
 
-cur.execute('''
+
+
+
+with open("skills.json", "w") as fp:
+    fp.write('{"skills":{ ')
+
+    for category in categories:
+        fp.write('\n"'+category+'":{')
+        id = 1
+
+        cur.execute('''
         SELECT skill.name, overtime_skills.counter
         FROM overtime_skills
         INNER JOIN skill ON skill.id=overtime_skills.skill_id
-        WHERE overtime_skills.language_id like 2 AND date_created like "2021-01-09"
-    ''')
+        WHERE overtime_skills.language_id like ? AND date_created like "2021-01-09" AND overtime_skills.counter > 1 LIMIT 20''',(categories.index(category)+1,))
+        rows = cur.fetchall()
 
-rows = cur.fetchall()
+
+        for row in rows:
+            (skill, counter) = row
+            fp.write('"'+skill+'":')
+
+
+            item = f'\"id\":\"{id}\", \"counter\" :\"{counter}\"'
+            if(len(rows) != id):
+                item = '{' + item + '}'
+            else:
+                item = '{' + item + '}'
+
+            fp.write(item)
+            if(len(rows) != id):
+                fp.write(',')
+            else:
+                fp.write('')
+
+            id += 1
+        if(len(categories) != categories.index(category)+1):
+            fp.write('},\n')
+        else:
+            fp.write('}\n')
+
+    fp.write('}}')
+
 
 # https://jsonbin.io/5fff1cb668f9f835a3dec1a1
 
-id = 1
+# id = 1
 
-with open("javascript.json", "w") as fp:
-    fp.write('{"js": [')
-    for row in rows:
-        (skill, counter) = row
-        print(skill, counter)
-        item = f'\"id\":\"{id}\", \"skill\":\"{skill}\", \"counter\" :\"{counter}\"'
-        item = '{' + item + '},'
+# with open("javascript.json", "w") as fp:
+#     fp.write('{"js": [')
+#     for row in rows:
+#         (skill, counter) = row
+#         print(skill, counter)
+#         item = f'\"id\":\"{id}\", \"skill\":\"{skill}\", \"counter\" :\"{counter}\"'
+#         item = '{' + item + '},'
 
-        fp.write(item)
-        id += 1
-    fp.write(']}')
+#         fp.write(item)
+#         id += 1
+#     fp.write(']}')
 
 
 # {
-#     "jokes": [
+    # "skills": []
+#     "javascript": [
 #     {
 #         "id": 1,
 #         "question": "Jaki jest ulubiony horror frontendowców?",
 #         "answer": "Freddy kontra JSON"
 #     },
-#     ]
+#     ],
 # }
 
 
